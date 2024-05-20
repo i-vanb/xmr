cf86e818edef57596f4301ec2ef36a3a10ee873c
Ivan B <ivan.giv14@gmail.com>
2024-05-20 19:14:13 +0300

    Logo in uppercase

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const Logo = ({path}:Props) => {
   return (
     <div>
-      <Link href={path} className="font-bold">XMRoomer</Link>
+      <Link href={path} className="font-bold">XMROOMER</Link>
     </div>
   )
 }
